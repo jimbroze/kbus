80ce134ca63907ff55b435efbad1146a854f814d
Jim Dickinson <jim.dickinson@opia.com>
2023-12-17 19:17:29 +0000

    Create Middleware Interface

---
+++ src/main/kotlin/Middleware.kt
@@ -0,0 +1,22 @@
+interface Middleware {
+    val messageId: String
+    fun <TMessage : Message> handle(message: TMessage, nextMiddleware: (TMessage) -> Any?): Any?
+    fun messageApplicable(message: Message): Boolean
+}
+
+fun <TMessage : Message> createMiddlewareChain(
+    finalHandler: (TMessage) -> Any?,
+    middlewares: List<Middleware>
+): (TMessage) -> Any? {
+//    fun handleMiddleware(
+//        middleware: Middleware,
+//        nextClosure: (TMessage) -> Any?,
+//        message: TMessage
+//    ): Any? = middleware.handle(message, nextClosure)
+
+    var nextHandler = finalHandler
+    middlewares.reversed().forEach { nextHandler = { message -> it.handle(message, nextHandler) } }
+//        nextHandler = { message -> handleMiddleware(thisMiddleware, nextHandler, message) }
+
+    return nextHandler
+}
