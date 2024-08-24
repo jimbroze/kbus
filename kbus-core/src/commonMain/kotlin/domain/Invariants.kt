3062c06270a7f86774a164d15147ba8484f198b6
Jim Dickinson <jim.dickinson@opia.com>
2024-08-24 17:09:21 +0100

    Catch InvalidInvariants in middleware
    These are rethrown as ResultFailureException which are caught and returned as a failed result

--- kbus-core/src/commonMain/kotlin/domain/Invariants.kt
+++ kbus-core/src/commonMain/kotlin/domain/Invariants.kt
@@ -1,5 +1,7 @@
 package com.jimbroze.kbus.core.domain
 
+import com.jimbroze.kbus.core.*
+
 open class InvalidInvariantException(override val message: String) : RuntimeException(message)
 
 abstract class HasInvariants {
@@ -8,5 +10,22 @@ abstract class HasInvariants {
     }
     protected fun assert(invariant: Boolean, exception: InvalidInvariantException) {
         if (!invariant) throw exception
+    }
+}
+
+interface InvariantCatchingMessage
+
+class InvalidInvariantCatcher : Middleware {
+    override suspend fun <TMessage : Message> handle(
+        message: TMessage,
+        nextMiddleware: MiddlewareHandler<TMessage>
+    ): Any? {
+        if (message !is InvariantCatchingMessage) return nextMiddleware(message)
+
+        return try {
+            nextMiddleware(message)
+        } catch (e: InvalidInvariantException) {
+            throw ResultFailureException(e)
+        }
     }
 }
---
+++ kbus-core/src/commonTest/kotlin/domain/InvariantsTest.kt
@@ -0,0 +1,68 @@
+package com.jimbroze.kbus.core.domain
+
+import com.jimbroze.kbus.core.*
+import kotlinx.coroutines.test.runTest
+import kotlin.test.*
+
+class InvalidInvariantExample(message: String) : InvalidInvariantException(message)
+
+open class InvalidInvariantsCommand(
+    val exception: InvalidInvariantException,
+) : Command()
+
+class InvalidInvariantsCatchingCommand(
+    exception: InvalidInvariantException,
+) : InvalidInvariantsCommand(exception), InvariantCatchingMessage
+
+class InvalidInvariantsCommandHandler : CommandHandler<InvalidInvariantsCommand, Unit> {
+    override suspend fun handle(message: InvalidInvariantsCommand) {
+        throw message.exception
+    }
+}
+
+class InvariantsTest {
+    @Test
+    fun invariant_catcher_only_processes_invariant_catching_message() = runTest {
+        val catcher = InvalidInvariantCatcher()
+
+        assertFailsWith<InvalidInvariantException>("Failure message"){
+            catcher.handle(
+                InvalidInvariantsCommand(
+                    InvalidInvariantException("Failure message")
+                )
+            ) {
+                InvalidInvariantsCommandHandler().handle(it)
+            }
+        }
+    }
+
+    @Test
+    fun invariant_catcher_converts_invalid_invariant_exception_to_result_failure() = runTest {
+        val catcher = InvalidInvariantCatcher()
+
+        assertFailsWith<ResultFailureException>("Failure message"){
+            catcher.handle(
+                InvalidInvariantsCatchingCommand(
+                    InvalidInvariantException("Failure message")
+                )
+            ) {
+                InvalidInvariantsCommandHandler().handle(it)
+            }
+        }
+    }
+
+    @Test
+    fun invariant_catcher_converts_invalid_invariant_exception_subclass_to_result_failure() = runTest {
+        val catcher = InvalidInvariantCatcher()
+
+        assertFailsWith<ResultFailureException>("Failure message"){
+            catcher.handle(
+                InvalidInvariantsCatchingCommand(
+                    InvalidInvariantExample("Failure message")
+                )
+            ) {
+                InvalidInvariantsCommandHandler().handle(it)
+            }
+        }
+    }
+}
