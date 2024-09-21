38b25b1d016383e262581d759a4afbc6a36f0557
Jim Dickinson <jim.dickinson@opia.com>
2024-09-21 18:52:40 +0100

    Add Detekt for code static analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,25 @@
 import com.ncorti.ktfmt.gradle.tasks.KtfmtFormatTask
+import io.gitlab.arturbosch.detekt.Detekt
 
-plugins { id("com.ncorti.ktfmt.gradle") version ("0.20.1") }
+plugins {
+    id("com.ncorti.ktfmt.gradle") version ("0.20.1")
+    id("io.gitlab.arturbosch.detekt") version ("1.23.7")
+}
 
 allprojects {
     group = "com.jimbroze"
 
     apply(plugin = "com.ncorti.ktfmt.gradle")
     ktfmt { kotlinLangStyle() }
+
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        source.from("src")
+        allRules = false
+        buildUponDefaultConfig = true
+        autoCorrect = true
+        parallel = true
+    }
 }
 
 val gitHooksDir = file(".git/hooks")
@@ -27,5 +40,19 @@ tasks.register<KtfmtFormatTask>("ktfmtPrecommitFormat") {
 tasks.whenTaskAdded {
     if (name == "build") {
         dependsOn("installGitHooks")
+    }
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        // Enable the generation of an HTML report
+        html.required.set(true)
+        html.outputLocation.set(file("build/reports/detekt.html"))
+
+        txt.required.set(true)
+        txt.outputLocation.set(file("build/reports/detekt.txt"))
+
+        md.required.set(true)
+        md.outputLocation.set(file("build/reports/detekt.md"))
     }
 }
