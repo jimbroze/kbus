840090d0f1532d1c0e0fbd7827d209e5d25f7e83
Jim Dickinson <jim.dickinson@opia.com>
2024-09-21 15:33:39 +0100

    Add ktfmt plugin for code formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
+plugins {
+    id("com.ncorti.ktfmt.gradle") version("0.20.1")
+}
+
 allprojects {
     group = "com.jimbroze"
+    apply(plugin = "com.ncorti.ktfmt.gradle")
+    ktfmt {
+        googleStyle()
+    }
 }
