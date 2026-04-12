c8449fbce3fe5bbfff66a6efa580e79cedc1a859
jimbroze <james.n.dickinson@gmail.com>
2026-04-12 10:37:04 +0100

    chore(build): update default version to 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 allprojects {
     group = "com.jimbroze"
-    version = System.getenv("VERSION_OVERRIDE") ?: "0.2.6-SNAPSHOT"
+    version = System.getenv("VERSION_OVERRIDE") ?: "0.3.0"
 
     apply(plugin = "com.ncorti.ktfmt.gradle")
     ktfmt { kotlinLangStyle() }
